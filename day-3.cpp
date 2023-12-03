cae2f3710b954a4184d0dd0fabd3be77793b6434
Giridhar Talla <giridhar.talla2002@gmail.com>
2023-12-03 18:26:38 +0000

    complete day3

--- day-3.cpp
+++ day-3.cpp
@@ -2,12 +2,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <regex>
 
 using namespace std;
+vector<string> data;
 
 vector<string> loadData() {
-    vector<string> data;
     string line;
     ifstream ip("ip.txt");
     if (ip.is_open()) {
@@ -22,30 +21,40 @@ vector<string> loadData() {
     return data;
 }
 
-bool isSymbol(int i, int j, vector<string> data) {
+bool isSymbol(int i, int j, int num, vector<vector<vector<int>>>& gears) {
     if (i < 0 || i >= data.size() || j < 0 || j >= data[i].length()) {
         return false;
     }
+
+    // start: part 2
+    if (data[i][j] == '*') {
+        gears[i][j].push_back(num);
+        // cout << "inside isSymbol: " << data[i][j] << " " << i << " " << j << " " << num << ' ' << gears[i][j].size() << endl;
+    }
+    // end
 
     return data[i][j] != '.' && !isdigit(data[i][j]);
 }
 
 int main() {
-    vector<string> data = loadData();
+    loadData();
     int sum = 0;
     int m = data[0].size();
+
+    vector<vector<vector<int>>> gears(data.size(), vector<vector<int>>(m));
 
     for(int i = 0; i < data.size(); i++) {
         int start = 0;
         string line = data[i];
 
-        cout << "i: " << i << " ";
+        // cout << "i: " << i << " ";
 
 
         for(int j = 0; j < m;){
             start = j;
             string num = "";
-            cout << "j: " << j << " ";
+            // cout << "j: " << j << " ";
+
             while (j < m && isdigit(line[j])) {
                 num += line[j];
                 j++;
@@ -58,22 +67,39 @@ int main() {
 
             int originalNum = stoi(num);
 
-            if(isSymbol(i, start-1, data) || isSymbol(i, j, data)) {
+            // if(isSymbol(i, start-1, data) || isSymbol(i, j, data)) {
                 // cout << "candidate: " << originalNum << endl;
-                sum += originalNum;
-                continue;
-            }
+                // sum += originalNum;
+                // continue;
+            // }
 
-            for(int k = start - 1; k < j+1; k++) {
-                cout << "k: " << k << " ";
-                if(isSymbol(i-1, k, data) || isSymbol(i+1, k, data)){
+            // for(int k = start - 1; k < j+1; k++) {
+                // cout << "k: " << k << " ";
+                // if(isSymbol(i-1, k, originalNum, gears, data) || isSymbol(i+1, k, originalNum, data)){
                     // cout << "candidate: " << originalNum << endl;
-                    sum += originalNum;
-                    break;
-                }
+                    // sum += originalNum;
+                    // break;
+                // }
+            // }
+
+            // start: part 2
+            isSymbol(i, start-1, originalNum, gears) || isSymbol(i, j, originalNum, gears);
+            for(int k = start - 1; k < j+1; k++) {
+                isSymbol(i-1, k, originalNum, gears) || isSymbol(i+1, k, originalNum, gears);
             }
-            cout << endl;
+            // end
             
+        }
+    }
+
+    vector<int> nums;
+
+    for(int i = 0; i < gears.size(); i++) {
+        for(int j = 0; j < gears[i].size(); j++) {
+            nums = gears[i][j];
+            if(data[i][j] == '*' && nums.size() == 2) {
+                sum += nums[0] * nums[1];
+            }
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <regex>
 
 using namespace std;
+vector<string> data;
 
 vector<string> loadData() {
-    vector<string> data;
     string line;
     ifstream ip("ip.txt");
     if (ip.is_open()) {
@@ -22,106 +21,8 @@ vector<string> loadData() {
     return data;
 }
 
-bool isSymbol(int i, int j, vector<string> data) {
-    if (i < 0 || i >= data.size() || j < 0 || j >= data[i].length()) {
-        return false;
-    }
-
-    return data[i][j] != '.' && !isdigit(data[i][j]);
-}
-
-bool isGear(int i, int j, vector<string> data) {
-    if (i < 0 || i >= data.size() || j < 0 || j >= data[i].length()) {
-        return false;
-    }
-
-    return data[i][j] == '*';
-}
-
 int main() {
-    vector<string> data = loadData();
-    int sum = 0;
-    int m = data[0].size();
+    loadData();
 
-    for(int i = 0; i < data.size(); i++) {
-        int start = 0;
-        string line = data[i];
-
-        // cout << "i: " << i << " ";
-
-
-        for(int j = 0; j < m;){
-            start = j;
-            string num1 = "", num2 = "";
-            // cout << "j: " << j << " ";
-
-            if(isGear(i, j, data)) {
-                if(isdigit(line[j+1])) {
-                    while(j < m && isdigit(line[j])) {
-                        if(num1 == "") {
-                            num1 += line[j];
-                        } else {
-                            num2 += line[j];
-                        }
-                        j++;
-                    }
-                } else if(isdigit(line[j-1])) {
-                    while(j >= 0 && isdigit(line[j])) {
-                        if(num1 == "") {
-                            num1 += line[j];
-                        } else {
-                            num2 += line[j];
-                        }
-                        j--;
-                    }
-                }
-                
-                j = start;
-
-                for(int k = start - 1; k < j+1; k++) {
-                    if(isSymbol(i-1, k, data) || isSymbol(i+1, k, data)){
-                        // cout << "candidate: " << originalNum << endl;
-                        sum += stoi(num1) * stoi(num2);
-                        break;
-                    }
-                }
-                
-            } else {
-                continue;
-            }
-
-            while (j < m && isdigit(line[j])) {
-                num += line[j];
-                j++;
-            }
-
-            if(num == "") {
-                j++;
-                continue;
-            }
-
-            int originalNum = stoi(num);
-
-            if(isSymbol(i, start-1, data) || isSymbol(i, j, data)) {
-                // cout << "candidate: " << originalNum << endl;
-                sum += originalNum;
-                continue;
-            }
-
-            for(int k = start - 1; k < j+1; k++) {
-                cout << "k: " << k << " ";
-                if(isSymbol(i-1, k, data) || isSymbol(i+1, k, data)){
-                    // cout << "candidate: " << originalNum << endl;
-                    sum += originalNum;
-                    break;
-                }
-            }
-            cout << endl;
-            
-        }
-    }
-
-
-    cout << "grand total: " << sum;
     return 0;
 }
