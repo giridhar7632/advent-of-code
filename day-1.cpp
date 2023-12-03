5fc844deb8052b364deb4d8d8d604dabeece0159
Giridhar Talla <giridhar.talla2002@gmail.com>
2023-12-03 11:00:08 +0530

    Update code

--- day-1.cpp
+++ day-1.cpp
@@ -2,25 +2,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
+map<string, int> numberMap = {
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+        {"zero", 0},
+    };
+
+int getCalibrationValue(string data) {
+    regex pattern(R"((\d+)|one|two|three|four|five|six|seven|eight|nine|zero)");
+    smatch matches;
+    vector<int> nums;
+
+    cout << data << endl;
+
+    for (sregex_iterator it(data.begin(), data.end(), pattern); it != sregex_iterator(); ++it) {
+        smatch match = *it;
+        string token = match.str();
+
+        auto temp = numberMap.find(token);
+        if (temp != numberMap.end()) {
+          nums.push_back(temp->second);
+            cout << "second: " << temp->second << " ";
+        } else if (all_of(token.begin(), token.end(), ::isdigit)) {
+            for (char digit : token) {
+                nums.push_back((int)digit - (int)'0');
+                cout << "digit: " << digit << " ";
+            }
+        }
+    }
+
+  cout << endl << nums.front()*10 + nums.back() << endl;
+  return (nums.front()*10 + nums.back());
+}
+
 int main() {
   string line;
-  vector<int> num;
   int sum = 0;
   ifstream ip("ip.txt");
   if (ip.is_open()) {
 
     while (getline(ip, line)) {
-      for (char c : line) {
-            if (c >= '0' && c <= '9') {
-                num.push_back((int)c - (int)'0');
-            }
-      }
+      // for (char c : line) {
+      //       if (c >= '0' && c <= '9') {
+      //           num.push_back((int)c - (int)'0');
+      //       }
+      // }
 
-      sum += (num.front()*10 + num.back());
-      num.clear();
+      sum += getCalibrationValue(line);
+      cout << "sum: " << sum << endl;
+      // sum += (num.front()*10 + num.back());
+      // num.clear();
     }
 
     ip.close();
