eae77959eda737829619ebacbc02dfc79b6c02d7
Giridhar Talla <giridhar.talla2002@gmail.com>
2023-12-27 07:43:04 +0000

    complete day 12

--- day-12.cpp
+++ day-12.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -20,20 +22,30 @@ void addMetadata(string line) {
   metadata.push_back(numbers);
 }
 
+string unfoldData(string line, bool isMeta) {
+  string repeated_parts = "";
+  for (int i = 0; i < 5; i++) {
+    repeated_parts += (i > 0 ? isMeta ? "," : "?" : "") + line;
+  }
+  return repeated_parts;
+}
+
 void loadData() {
   string line;
   ifstream ip("ip.txt");
   if (ip.is_open()) {
 
     while (getline(ip, line)) {
-      data.push_back(line.substr(0, line.find(" ")));
-      addMetadata(line.substr(line.find(" ") + 1));
+      data.push_back(unfoldData(line.substr(0, line.find(" ")), 0));
+      addMetadata(unfoldData(line.substr(line.find(" ") + 1), 1));
     }
 
     ip.close();
   }
 }
 
+/*
+// for part-1
 bool isValid(vector<int> &line, vector<int> &target) {
   int n = line.size();
   vector<int> runs;
@@ -62,6 +74,7 @@ bool isValid(vector<int> &line, vector<int> &target) {
 }
 
 int noOfWays(string &s, vector<int> &target) {
+
   vector<int> line;
   vector<int> idxs;
   for (int i = 0; i < s.length(); i++) {
@@ -95,14 +108,118 @@ int noOfWays(string &s, vector<int> &target) {
     }
   }
   return cnt;
+
+}
+*/
+
+long long noOfWays(string &s, vector<int> targetRuns) {
+  targetRuns.push_back(0);
+  int maxRun = *max_element(targetRuns.begin(), targetRuns.end());
+  s += '.';
+
+  int n = s.length();
+  int m = targetRuns.size();
+  vector<vector<vector<long long>>> dp(
+      n, vector<vector<long long>>(m, vector<long long>(maxRun + 1, -1)));
+
+  for (int i = 0; i < n; i++) {
+    char x = s[i];
+
+    for (int j = 0; j < m; j++) {
+      for (int k = 0; k <= targetRuns[j]; k++) {
+        // base case
+        if (i == 0) {
+          if (j != 0) {
+            dp[i][j][k] = 0;
+            continue;
+          }
+
+          if (x == '#') {
+            if (k != 1) {
+              dp[i][j][k] = 0;
+              continue;
+            }
+
+            dp[i][j][k] = 1;
+            continue;
+          }
+
+          if (x == '.') {
+            if (k != 0) {
+              dp[i][j][k] = 0;
+              continue;
+            }
+
+            dp[i][j][k] = 1;
+            continue;
+          }
+
+          if (x == '?') {
+            if (k < 0 || k > 1) {
+              dp[i][j][k] = 0;
+              continue;
+            }
+
+            dp[i][j][k] = 1;
+            continue;
+          }
+        }
+
+        // process for '.'
+        long long ansDot;
+        if (k != 0) {
+          ansDot = 0;
+        } else if (j > 0) {
+          assert(k == 0);
+          ansDot = dp[i - 1][j - 1][targetRuns[j - 1]];
+          ansDot += dp[i - 1][j][0];
+        } else {
+          ansDot = (s.substr(0, i).find("#") == string::npos) ? 1 : 0;
+        }
+
+        // process for '#'
+        long long ansHash;
+        if (k == 0) {
+          ansHash = 0;
+        } else {
+          ansHash = dp[i - 1][j][k - 1];
+        }
+
+        if (x == '.') {
+          dp[i][j][k] = ansDot;
+        } else if (x == '#') {
+          dp[i][j][k] = ansHash;
+        } else {
+          dp[i][j][k] = ansDot + ansHash;
+        }
+      }
+    }
+  }
+
+  return dp[n - 1][m - 1][0];
 }
 
 int main() {
   loadData();
-  int sum = 0;
+  long long sum = 0;
+
+  // for (const auto &line : data) {
+  //   cout << line << endl;
+  // }
+
+  // for (const auto &line : metadata) {
+  //   for (int i : line) {
+  //     cout << i << " ";
+  //   }
+  //   cout << endl;
+  // }
 
   for (int i = 0; i < data.size(); i++) {
-    sum += noOfWays(data[i], metadata[i]);
+    long long ans = noOfWays(data[i], metadata[i]);
+    if (ans < 0) {
+      cout << data[i] << " : " << ans << endl;
+    }
+    sum += ans;
   }
 
   cout << "grand total: " << sum;
